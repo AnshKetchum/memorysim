93735a1c2474028e52b432b09fdc96eb36ab1408
Ansh <eye.am.ansh@gmail.com>
2025-09-27 18:56:52 +0000

    Scope testing makefile into smaller instances

--- Makefrags/test.mk
+++ Makefrags/test.mk
@@ -1,13 +1,19 @@
-test:
+unit-test:
 	echo "Running unit tests"
 	sbt "memctrl/test"
+
+verilator-trace-tests:
+	echo "Building verilator traces for testing"
+	$(MAKE) verilator-trace
+	pytest tests/ -v -s
+
+test:
+	$(MAKE) unit-test
 
 	echo "Running verilator sanity tests"
 	$(MAKE) verilator-sanity-test
 	$(MAKE) build-clean
 
-	echo "Building verilator traces for testing"
-	$(MAKE) verilator-trace
-	pytest tests/ -v -s
+	$(MAKE) verilator-trace-tests
 
 	$(MAKE) build-clean
